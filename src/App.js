0a9abbccf2629a1933c4b0243682d69123b0e2a6
ehidiamhenwinter@esp.sn <ehidiamhenwinter@esp.sn>
2026-04-29 20:59:47 +0000

    Setup initial SenTransport avec Header

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Statistique from './Statistique';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="contenu">
+        <p>Bienvenue ! Cette application vous aide a trouver
+        votre ligne de bus a Dakar.</p>
+        <div className="stats">
+          <Statistique />
+          <Statistique />
+          <Statistique />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
